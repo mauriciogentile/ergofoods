494711f2198e7aa6468bc47a49ceb6668d93c7b1
Gentile, Mauricio <mgentile@IADB.ORG>
2021-04-11 21:03:13 -0400

    https + google

--- src/public_html/index.php
+++ src/public_html/index.php
@@ -1,4 +1,12 @@
 <?php include 'layout/head.php'; ?>
+<?php
+	if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
+		$location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+		header('HTTP/1.1 301 Moved Permanently');
+		header('Location: ' . $location);
+		exit;
+	}
+?>
 <?php include 'layout/header-eng.php'; ?>
   <main class="o-main | js-main">
     <section id="h-1" class="o-layout show-me | js-hero">
--- src/public_html/layout/head.php
+++ src/public_html/layout/head.php
@@ -12,5 +12,14 @@
   <link rel="stylesheet" href="./css/slick.css">
   <link rel="stylesheet" href="./css/fontawesome-all.min.css">
   <link rel="stylesheet" href="./css/main.css">
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-952FNT9D4C"></script>
+  <script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-952FNT9D4C');
+  </script>
 </head>
 <body>
